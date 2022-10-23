36d7c63d0998d6ea1f1089002151a1fe6d2a2df8
Jerri Ahonen <jerri.ahonen@gmail.com>
2022-10-23 16:56:09 +0300

    Add side thrusters

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,16 @@ using Vector3 = UnityEngine.Vector3;
 public class Player : MonoBehaviour
 {
 	[SerializeField] private float movementForce;
+	[SerializeField] private float boostForce;
 	[SerializeField] private float rotationForce;
 	[SerializeField] private float collisionForce;
+	[SerializeField] private ParticleSystem rearThrustPS;
+	[SerializeField] private ParticleSystem rightThrustPS;
+	[SerializeField] private ParticleSystem leftThrustPS;
+	
 	private Rigidbody rb;
+	private bool turningRight;
+	private bool turningLeft;
 
 	private void Awake()
 	{
@@ -16,18 +23,57 @@ public class Player : MonoBehaviour
 
 	private void Update()
 	{
-		if (Input.GetKey(KeyCode.A))
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			turningLeft = true;
+			rightThrustPS.Play();
+			leftThrustPS.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+		}
+
+		if (Input.GetKeyUp(KeyCode.A))
+		{
+			turningLeft = false;
+			rightThrustPS.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+		}
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			turningRight = true;
+			leftThrustPS.Play();
+			rightThrustPS.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+		}
+
+		if (Input.GetKeyUp(KeyCode.D))
+		{
+			turningRight = false;
+			leftThrustPS.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+		}
+		
+		if (turningLeft)
 		{
 			// Rotate left
 			rb.AddTorque(Vector3.up * -rotationForce);
 		}
-		else if (Input.GetKey(KeyCode.D))
+		
+		if (turningRight)
 		{
 			// Rotate right
 			rb.AddTorque(Vector3.up * rotationForce);
 		}
+
+		var finalMovementForce = movementForce;
+		var rearThrustPSMain = rearThrustPS.main;
+		if (Input.GetKey(KeyCode.Space))
+		{
+			finalMovementForce = boostForce;
+			rearThrustPSMain.startSize = 3f;
+		}
+		else
+		{
+			rearThrustPSMain.startSize = 1f;
+		}
 		
-		rb.AddForce(transform.forward * movementForce, ForceMode.Force);
+		rb.AddForce(transform.forward * finalMovementForce, ForceMode.Force);
 	}
 
 	private void OnCollisionEnter(Collision collision)
